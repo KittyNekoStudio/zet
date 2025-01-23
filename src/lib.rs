9b99abcc041a8f6827165e2b2823cd11f8ffe12b
kittycat <kittynekostudio@gmail.com>
2025-01-23 11:13:11 -0800

    Some stuff.

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
+
+fn open_config(filename: String) -> std::io::Result<File> {
+    let filename = format!("{}/.{filename}", env::var("HOME").unwrap());
+    let file = File::open(filename)?;
+
+    Ok(file)
+}
+
+fn get_string(mut file: File, target: &str) -> Result<String, ()> {
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+
+    let target = buffer.lines().find_map(|s| if s.contains(target) {Some(s)} else {None});
+
+    if target.is_some() {
+        return Ok(target.unwrap().to_string());
+    }
+
+    Err(())
+}
+
+// TODO! refactor this to take any arbitrary string
+fn remove_whitespace_after_euqal(string: &str) -> String {
+    let whitespace_end = string.char_indices().find_map(|(idx, c)| 
+        if c != '=' {None} else {Some(idx + 1)})
+        .unwrap_or_else(|| string.len());
+
+    string[whitespace_end..].to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_read_string_from_file() {
+        let file = open_config("foo.conf".to_string());
+        assert_eq!(true, file.is_ok());
+
+        let string = get_string(file.unwrap(), "Hello, World!");
+        assert_eq!(string, Ok("Hello, World! Testing.".to_string()));
+    }
+    #[test]
+    fn test_remove_whitespace_from_beginning() {
+        let string = "Hello = World!".to_string();
+        assert_eq!(" World!", remove_whitespace_after_euqal(&string));
+
+        let string = "path = /hi/there".to_string();
+        assert_eq!(" /hi/there", remove_whitespace_after_euqal(&string));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use chrono::Local;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> std::io::Result<()> {
+    let format = Local::now().format("%Y%m%d%H%M").to_string();
+    let args: Vec<String> = env::args().collect();
+    let mut filename = format.clone();
+    if args.len() > 1 {
+        for i in 1..args.len() {
+            let arg = {
+            let mut temp = args[i].clone();
+            temp.insert_str(0, "-");
+            temp
+            };
+            filename += &arg;
+        }
+    }
+
+    let filepath = PathBuf::from(env::var("HOME").expect("I'm changing this soon")
+        + "/Zettelkasten/" + &filename + ".md");
+
+    let mut file = File::create(&filepath)?;
+
+    filename = filename.replace("-", " ");
+
+    file.write_all(filename.as_bytes())?;
+
+    std::process::Command::new("vim")
+        .arg(&filepath.into_os_string().into_string().unwrap())
+        .spawn()
+        .expect("Error: Failed to run editor")
+        .wait()
+        .expect("Error: Editor returned a non-zero status");
+
+
+    Ok(())
+}
